c85d3be12a8b33f9ed99170092cdab641f130c54
Charlez Yuan Venenoso <saikopewds@gmail.com>
2026-03-28 21:42:08 +0800

    success prescription

--- frmPrescriptionEntry.Designer.vb
+++ frmPrescriptionEntry.Designer.vb
@@ -24,14 +24,20 @@ Partial Class frmPrescriptionEntry
     Private Sub InitializeComponent()
         Me.lblPrescriptionID = New System.Windows.Forms.Label()
         Me.txtPrescriptionID = New System.Windows.Forms.TextBox()
-        Me.lblPhysicianID = New System.Windows.Forms.Label()
-        Me.txtPhysicianID = New System.Windows.Forms.TextBox()
-        Me.lblPatientID = New System.Windows.Forms.Label()
-        Me.txtPatientID = New System.Windows.Forms.TextBox()
+        Me.lblPhysician = New System.Windows.Forms.Label()
+        Me.cboPhysician = New System.Windows.Forms.ComboBox()
+        Me.lblPatient = New System.Windows.Forms.Label()
+        Me.cboPatient = New System.Windows.Forms.ComboBox()
         Me.lblInstruction = New System.Windows.Forms.Label()
         Me.txtInstruction = New System.Windows.Forms.TextBox()
         Me.lblPrescriptionDate = New System.Windows.Forms.Label()
         Me.dtpPrescriptionDate = New System.Windows.Forms.DateTimePicker()
+        Me.lblMedicine = New System.Windows.Forms.Label()
+        Me.cboMedicine = New System.Windows.Forms.ComboBox()
+        Me.btnAddMedicine = New System.Windows.Forms.Button()
+        Me.btnRemoveMedicine = New System.Windows.Forms.Button()
+        Me.lblSelectedMedicines = New System.Windows.Forms.Label()
+        Me.lstSelectedMedicines = New System.Windows.Forms.ListBox()
         Me.btnSave = New System.Windows.Forms.Button()
         Me.btnCancel = New System.Windows.Forms.Button()
         Me.SuspendLayout()
@@ -53,35 +59,43 @@ Partial Class frmPrescriptionEntry
         Me.txtPrescriptionID.TabIndex = 1
         Me.txtPrescriptionID.TabStop = False
         '
-        'lblPhysicianID
+        'lblPhysician
         '
-        Me.lblPhysicianID.Location = New System.Drawing.Point(20, 62)
-        Me.lblPhysicianID.Name = "lblPhysicianID"
-        Me.lblPhysicianID.Size = New System.Drawing.Size(130, 24)
-        Me.lblPhysicianID.TabIndex = 2
-        Me.lblPhysicianID.Text = "Physician ID *:"
+        Me.lblPhysician.Location = New System.Drawing.Point(20, 62)
+        Me.lblPhysician.Name = "lblPhysician"
+        Me.lblPhysician.Size = New System.Drawing.Size(130, 24)
+        Me.lblPhysician.TabIndex = 2
+        Me.lblPhysician.Text = "Physician *:"
         '
-        'txtPhysicianID
+        'cboPhysician
         '
-        Me.txtPhysicianID.Location = New System.Drawing.Point(160, 58)
-        Me.txtPhysicianID.Name = "txtPhysicianID"
-        Me.txtPhysicianID.Size = New System.Drawing.Size(330, 20)
-        Me.txtPhysicianID.TabIndex = 3
+        Me.cboPhysician.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cboPhysician.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cboPhysician.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown
+        Me.cboPhysician.FormattingEnabled = True
+        Me.cboPhysician.Location = New System.Drawing.Point(160, 58)
+        Me.cboPhysician.Name = "cboPhysician"
+        Me.cboPhysician.Size = New System.Drawing.Size(330, 21)
+        Me.cboPhysician.TabIndex = 3
         '
-        'lblPatientID
+        'lblPatient
         '
-        Me.lblPatientID.Location = New System.Drawing.Point(20, 100)
-        Me.lblPatientID.Name = "lblPatientID"
-        Me.lblPatientID.Size = New System.Drawing.Size(130, 24)
-        Me.lblPatientID.TabIndex = 4
-        Me.lblPatientID.Text = "Patient ID *:"
+        Me.lblPatient.Location = New System.Drawing.Point(20, 100)
+        Me.lblPatient.Name = "lblPatient"
+        Me.lblPatient.Size = New System.Drawing.Size(130, 24)
+        Me.lblPatient.TabIndex = 4
+        Me.lblPatient.Text = "Patient *:"
         '
-        'txtPatientID
+        'cboPatient
         '
-        Me.txtPatientID.Location = New System.Drawing.Point(160, 96)
-        Me.txtPatientID.Name = "txtPatientID"
-        Me.txtPatientID.Size = New System.Drawing.Size(330, 20)
-        Me.txtPatientID.TabIndex = 5
+        Me.cboPatient.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cboPatient.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cboPatient.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown
+        Me.cboPatient.FormattingEnabled = True
+        Me.cboPatient.Location = New System.Drawing.Point(160, 96)
+        Me.cboPatient.Name = "cboPatient"
+        Me.cboPatient.Size = New System.Drawing.Size(330, 21)
+        Me.cboPatient.TabIndex = 5
         '
         'lblInstruction
         '
@@ -115,23 +129,76 @@ Partial Class frmPrescriptionEntry
         Me.dtpPrescriptionDate.Size = New System.Drawing.Size(330, 20)
         Me.dtpPrescriptionDate.TabIndex = 9
         '
+        'lblMedicine
+        '
+        Me.lblMedicine.Location = New System.Drawing.Point(20, 278)
+        Me.lblMedicine.Name = "lblMedicine"
+        Me.lblMedicine.Size = New System.Drawing.Size(130, 24)
+        Me.lblMedicine.TabIndex = 10
+        Me.lblMedicine.Text = "Medicine *:"
+        '
+        'cboMedicine
+        '
+        Me.cboMedicine.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cboMedicine.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
+        Me.cboMedicine.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDown
+        Me.cboMedicine.FormattingEnabled = True
+        Me.cboMedicine.Location = New System.Drawing.Point(160, 274)
+        Me.cboMedicine.Name = "cboMedicine"
+        Me.cboMedicine.Size = New System.Drawing.Size(230, 21)
+        Me.cboMedicine.TabIndex = 11
+        '
+        'btnAddMedicine
+        '
+        Me.btnAddMedicine.Location = New System.Drawing.Point(396, 272)
+        Me.btnAddMedicine.Name = "btnAddMedicine"
+        Me.btnAddMedicine.Size = New System.Drawing.Size(94, 24)
+        Me.btnAddMedicine.TabIndex = 12
+        Me.btnAddMedicine.Text = "Add Medicine"
+        Me.btnAddMedicine.UseVisualStyleBackColor = True
+        '
+        'btnRemoveMedicine
+        '
+        Me.btnRemoveMedicine.Location = New System.Drawing.Point(396, 305)
+        Me.btnRemoveMedicine.Name = "btnRemoveMedicine"
+        Me.btnRemoveMedicine.Size = New System.Drawing.Size(94, 24)
+        Me.btnRemoveMedicine.TabIndex = 13
+        Me.btnRemoveMedicine.Text = "Remove"
+        Me.btnRemoveMedicine.UseVisualStyleBackColor = True
+        '
+        'lblSelectedMedicines
+        '
+        Me.lblSelectedMedicines.Location = New System.Drawing.Point(20, 307)
+        Me.lblSelectedMedicines.Name = "lblSelectedMedicines"
+        Me.lblSelectedMedicines.Size = New System.Drawing.Size(130, 24)
+        Me.lblSelectedMedicines.TabIndex = 14
+        Me.lblSelectedMedicines.Text = "Selected Medicines *:"
+        '
+        'lstSelectedMedicines
+        '
+        Me.lstSelectedMedicines.FormattingEnabled = True
+        Me.lstSelectedMedicines.Location = New System.Drawing.Point(160, 305)
+        Me.lstSelectedMedicines.Name = "lstSelectedMedicines"
+        Me.lstSelectedMedicines.Size = New System.Drawing.Size(230, 95)
+        Me.lstSelectedMedicines.TabIndex = 15
+        '
         'btnSave
         '
         Me.btnSave.BackColor = System.Drawing.Color.DarkBlue
         Me.btnSave.ForeColor = System.Drawing.Color.White
-        Me.btnSave.Location = New System.Drawing.Point(300, 282)
+        Me.btnSave.Location = New System.Drawing.Point(300, 418)
         Me.btnSave.Name = "btnSave"
         Me.btnSave.Size = New System.Drawing.Size(90, 34)
-        Me.btnSave.TabIndex = 10
+        Me.btnSave.TabIndex = 16
         Me.btnSave.Text = "Save"
         Me.btnSave.UseVisualStyleBackColor = False
         '
         'btnCancel
         '
-        Me.btnCancel.Location = New System.Drawing.Point(400, 282)
+        Me.btnCancel.Location = New System.Drawing.Point(400, 418)
         Me.btnCancel.Name = "btnCancel"
         Me.btnCancel.Size = New System.Drawing.Size(90, 34)
-        Me.btnCancel.TabIndex = 11
+        Me.btnCancel.TabIndex = 17
         Me.btnCancel.Text = "Cancel"
         Me.btnCancel.UseVisualStyleBackColor = True
         '
@@ -139,17 +206,23 @@ Partial Class frmPrescriptionEntry
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(520, 340)
+        Me.ClientSize = New System.Drawing.Size(520, 470)
+        Me.Controls.Add(Me.lstSelectedMedicines)
+        Me.Controls.Add(Me.lblSelectedMedicines)
+        Me.Controls.Add(Me.btnRemoveMedicine)
+        Me.Controls.Add(Me.btnAddMedicine)
+        Me.Controls.Add(Me.cboMedicine)
+        Me.Controls.Add(Me.lblMedicine)
         Me.Controls.Add(Me.btnCancel)
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.dtpPrescriptionDate)
         Me.Controls.Add(Me.lblPrescriptionDate)
         Me.Controls.Add(Me.txtInstruction)
         Me.Controls.Add(Me.lblInstruction)
-        Me.Controls.Add(Me.txtPatientID)
-        Me.Controls.Add(Me.lblPatientID)
-        Me.Controls.Add(Me.txtPhysicianID)
-        Me.Controls.Add(Me.lblPhysicianID)
+        Me.Controls.Add(Me.cboPatient)
+        Me.Controls.Add(Me.lblPatient)
+        Me.Controls.Add(Me.cboPhysician)
+        Me.Controls.Add(Me.lblPhysician)
         Me.Controls.Add(Me.txtPrescriptionID)
         Me.Controls.Add(Me.lblPrescriptionID)
         Me.Name = "frmPrescriptionEntry"
@@ -162,14 +235,20 @@ Partial Class frmPrescriptionEntry
 
     Friend WithEvents lblPrescriptionID As Label
     Friend WithEvents txtPrescriptionID As TextBox
-    Friend WithEvents lblPhysicianID As Label
-    Friend WithEvents txtPhysicianID As TextBox
-    Friend WithEvents lblPatientID As Label
-    Friend WithEvents txtPatientID As TextBox
+    Friend WithEvents lblPhysician As Label
+    Friend WithEvents cboPhysician As ComboBox
+    Friend WithEvents lblPatient As Label
+    Friend WithEvents cboPatient As ComboBox
     Friend WithEvents lblInstruction As Label
     Friend WithEvents txtInstruction As TextBox
     Friend WithEvents lblPrescriptionDate As Label
     Friend WithEvents dtpPrescriptionDate As DateTimePicker
+    Friend WithEvents lblMedicine As Label
+    Friend WithEvents cboMedicine As ComboBox
+    Friend WithEvents btnAddMedicine As Button
+    Friend WithEvents btnRemoveMedicine As Button
+    Friend WithEvents lblSelectedMedicines As Label
+    Friend WithEvents lstSelectedMedicines As ListBox
     Friend WithEvents btnSave As Button
     Friend WithEvents btnCancel As Button
 End Class
--- frmPrescriptionEntry.vb
+++ frmPrescriptionEntry.vb
@@ -3,6 +3,16 @@
 Partial Class frmPrescriptionEntry
 
     Private ReadOnly _connectionString As String
+    Private ReadOnly _selectedMedicines As New List(Of MedicineSelectionItem)()
+
+    Private Class MedicineSelectionItem
+        Public Property MedicineID As Integer
+        Public Property DisplayName As String
+
+        Public Overrides Function ToString() As String
+            Return DisplayName
+        End Function
+    End Class
 
     Public Sub New()
         InitializeComponent()
@@ -15,11 +25,72 @@ Partial Class frmPrescriptionEntry
 
     Private Sub frmPrescriptionEntry_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         LoadNextPrescriptionId()
+        LoadPhysicianAndPatientOptions()
+        LoadMedicineOptions()
+        RefreshSelectedMedicinesList()
     End Sub
 
     Private Sub btnCancel_Click(sender As Object, e As EventArgs) Handles btnCancel.Click
         Me.DialogResult = DialogResult.Cancel
         Me.Close()
+    End Sub
+
+    Private Sub btnAddMedicine_Click(sender As Object, e As EventArgs) Handles btnAddMedicine.Click
+        If cboMedicine.SelectedIndex < 0 OrElse cboMedicine.SelectedValue Is Nothing OrElse TypeOf cboMedicine.SelectedValue Is DataRowView Then
+            MessageBox.Show("Select a medicine to add.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            cboMedicine.Focus()
+            Return
+        End If
+
+        Dim medicineId As Integer = Convert.ToInt32(cboMedicine.SelectedValue)
+        If IsMedicineAlreadySelected(medicineId) Then
+            MessageBox.Show("Medicine is already added to this prescription.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+        _selectedMedicines.Add(New MedicineSelectionItem With {
+            .MedicineID = medicineId,
+            .DisplayName = cboMedicine.Text.Trim()
+        })
+
+        RefreshSelectedMedicinesList()
+        cboMedicine.SelectedIndex = -1
+    End Sub
+
+    Private Sub btnRemoveMedicine_Click(sender As Object, e As EventArgs) Handles btnRemoveMedicine.Click
+        If lstSelectedMedicines.SelectedItem Is Nothing Then
+            MessageBox.Show("Select a medicine from the list to remove.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            lstSelectedMedicines.Focus()
+            Return
+        End If
+
+        Dim selectedItem As MedicineSelectionItem = TryCast(lstSelectedMedicines.SelectedItem, MedicineSelectionItem)
+        If selectedItem Is Nothing Then Return
+
+        For i As Integer = _selectedMedicines.Count - 1 To 0 Step -1
+            If _selectedMedicines(i).MedicineID = selectedItem.MedicineID Then
+                _selectedMedicines.RemoveAt(i)
+                Exit For
+            End If
+        Next
+
+        RefreshSelectedMedicinesList()
+    End Sub
+
+    Private Function IsMedicineAlreadySelected(medicineId As Integer) As Boolean
+        For Each item As MedicineSelectionItem In _selectedMedicines
+            If item.MedicineID = medicineId Then Return True
+        Next
+
+        Return False
+    End Function
+
+    Private Sub RefreshSelectedMedicinesList()
+        lstSelectedMedicines.Items.Clear()
+
+        For Each item As MedicineSelectionItem In _selectedMedicines
+            lstSelectedMedicines.Items.Add(item)
+        Next
     End Sub
 
     Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
@@ -28,25 +99,14 @@ Partial Class frmPrescriptionEntry
         End If
 
         Dim prescriptionId As Integer
-        Dim physicianId As Integer
-        Dim patientId As Integer
 
         If Not Integer.TryParse(txtPrescriptionID.Text.Trim(), prescriptionId) Then
             MessageBox.Show("Invalid Prescription ID.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
             Return
         End If
 
-        If Not Integer.TryParse(txtPhysicianID.Text.Trim(), physicianId) Then
-            MessageBox.Show("Physician ID must be numeric.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            txtPhysicianID.Focus()
-            Return
-        End If
-
-        If Not Integer.TryParse(txtPatientID.Text.Trim(), patientId) Then
-            MessageBox.Show("Patient ID must be numeric.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            txtPatientID.Focus()
-            Return
-        End If
+        Dim physicianId As Integer = Convert.ToInt32(cboPhysician.SelectedValue)
+        Dim patientId As Integer = Convert.ToInt32(cboPatient.SelectedValue)
 
         Try
             Using conn As New MySqlConnection(_connectionString)
@@ -63,6 +123,15 @@ Partial Class frmPrescriptionEntry
                     cmd.Parameters.AddWithValue("@PrescriptionDate", dtpPrescriptionDate.Value.Date)
                     cmd.ExecuteNonQuery()
                 End Using
+
+                Dim inclusionSql As String = "INSERT INTO prescription_inclusion (MedicineID, PrescriptionID) VALUES (@MedicineID, @PrescriptionID)"
+                For Each medicine As MedicineSelectionItem In _selectedMedicines
+                    Using inclusionCmd As New MySqlCommand(inclusionSql, conn)
+                        inclusionCmd.Parameters.AddWithValue("@MedicineID", medicine.MedicineID)
+                        inclusionCmd.Parameters.AddWithValue("@PrescriptionID", prescriptionId)
+                        inclusionCmd.ExecuteNonQuery()
+                    End Using
+                Next
             End Using
 
             MessageBox.Show("Prescription added successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
@@ -84,15 +153,15 @@ Partial Class frmPrescriptionEntry
             Return False
         End If
 
-        If String.IsNullOrWhiteSpace(txtPhysicianID.Text) Then
-            MessageBox.Show("Physician ID is required.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            txtPhysicianID.Focus()
+        If cboPhysician.SelectedIndex < 0 OrElse cboPhysician.SelectedValue Is Nothing OrElse TypeOf cboPhysician.SelectedValue Is DataRowView Then
+            MessageBox.Show("Physician is required.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            cboPhysician.Focus()
             Return False
         End If
 
-        If String.IsNullOrWhiteSpace(txtPatientID.Text) Then
-            MessageBox.Show("Patient ID is required.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
-            txtPatientID.Focus()
+        If cboPatient.SelectedIndex < 0 OrElse cboPatient.SelectedValue Is Nothing OrElse TypeOf cboPatient.SelectedValue Is DataRowView Then
+            MessageBox.Show("Patient is required.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            cboPatient.Focus()
             Return False
         End If
 
@@ -102,8 +171,87 @@ Partial Class frmPrescriptionEntry
             Return False
         End If
 
+        If _selectedMedicines.Count = 0 Then
+            MessageBox.Show("Add at least one medicine to the prescription.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            cboMedicine.Focus()
+            Return False
+        End If
+
         Return True
     End Function
+
+    Private Sub LoadPhysicianAndPatientOptions()
+        If String.IsNullOrWhiteSpace(_connectionString) Then
+            cboPhysician.DataSource = Nothing
+            cboPatient.DataSource = Nothing
+            Return
+        End If
+
+        Try
+            Using conn As New MySqlConnection(_connectionString)
+                conn.Open()
+
+                Dim physicianTable As New DataTable()
+                Dim physicianSql As String = "SELECT PhysicianID, CONCAT(FirstName, ' ', LastName, ' (', PhysicianID, ')') AS FullName FROM physician ORDER BY FirstName, LastName"
+                Using physicianCmd As New MySqlCommand(physicianSql, conn)
+                    Using physicianAdapter As New MySqlDataAdapter(physicianCmd)
+                        physicianAdapter.Fill(physicianTable)
+                    End Using
+                End Using
+
+                cboPhysician.DataSource = physicianTable
+                cboPhysician.DisplayMember = "FullName"
+                cboPhysician.ValueMember = "PhysicianID"
+                cboPhysician.SelectedIndex = -1
+
+                Dim patientTable As New DataTable()
+                Dim patientSql As String = "SELECT PatientID, CONCAT(FirstName, ' ', LastName, ' (', PatientID, ')') AS FullName FROM patient ORDER BY FirstName, LastName"
+                Using patientCmd As New MySqlCommand(patientSql, conn)
+                    Using patientAdapter As New MySqlDataAdapter(patientCmd)
+                        patientAdapter.Fill(patientTable)
+                    End Using
+                End Using
+
+                cboPatient.DataSource = patientTable
+                cboPatient.DisplayMember = "FullName"
+                cboPatient.ValueMember = "PatientID"
+                cboPatient.SelectedIndex = -1
+            End Using
+        Catch ex As Exception
+            cboPhysician.DataSource = Nothing
+            cboPatient.DataSource = Nothing
+            MessageBox.Show("Unable to load patient and physician names: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub LoadMedicineOptions()
+        If String.IsNullOrWhiteSpace(_connectionString) Then
+            cboMedicine.DataSource = Nothing
+            Return
+        End If
+
+        Try
+            Using conn As New MySqlConnection(_connectionString)
+                conn.Open()
+
+                Dim medicineTable As New DataTable()
+                Dim medicineSql As String = "SELECT MedicineID, CONCAT(MedicineName, ' (', MedicineID, ')') AS FullName FROM medicine ORDER BY MedicineName"
+                Using medicineCmd As New MySqlCommand(medicineSql, conn)
+                    Using medicineAdapter As New MySqlDataAdapter(medicineCmd)
+                        medicineAdapter.Fill(medicineTable)
+                    End Using
+                End Using
+
+                cboMedicine.DataSource = medicineTable
+                cboMedicine.DisplayMember = "FullName"
+                cboMedicine.ValueMember = "MedicineID"
+                cboMedicine.SelectedIndex = -1
+            End Using
+        Catch ex As Exception
+            cboMedicine.DataSource = Nothing
+            MessageBox.Show("Unable to load medicines: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
 
     Private Sub LoadNextPrescriptionId()
         If String.IsNullOrWhiteSpace(_connectionString) Then
